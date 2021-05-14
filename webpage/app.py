6278cab52be2b0ca40c02fbb7a43325246a4c31c
Rasmus Aagaard <raagaard97@gmail.com>
2021-05-14 23:02:43 +0200

    summary

--- webpage/app.py
+++ webpage/app.py
@@ -35,16 +35,16 @@ app.layout = html.Div(
                 dcc.Markdown('''
                 # About This Webpage 
                 
-                Hi, and welcome to our website made for our final project in the course []()  <a href="https://kurser.dtu.dk/course/02806" target="_blank">02806 Social Data Science and Visualization!</a>
+                Hi, and welcome to our website made for our final project in the course [02806 Social Data Science and Visualization!](https://kurser.dtu.dk/course/02806) (To open links, you may need to right click->Open in new tab because of Dash weirdness.)
                 
-                Our project explores vehicle collisions in New York City using a <a href="https://data.cityofnewyork.us/Public-Safety/Motor-Vehicle-Collisions-Crashes/h9gi-nx95" target="_blank">data set</a> provided by the New York Police Department (NYPD).
+                Our project explores vehicle collisions in New York City using a [data set](https://data.cityofnewyork.us/Public-Safety/Motor-Vehicle-Collisions-Crashes/h9gi-nx95) provided by the New York Police Department (NYPD).
                 
                 The problem of car collisions in New York city is estimated to cost the city's economy $4 billion yearly when taking everything from medical expenses to property damage into account. We would like to help with understanding *when* and *where* this problem occurs as that is often the first step to prevention. We will also look at a few selected topics of interest to analyse further. 
 
                 We encourage you, the reader, to interact with our visualizations to hopefully get the full experience and learn something new and exciting.
                 ## Defining Serious Collisions
 
-                For this project, we defined serious collisions as collisions where people are either injured or killed. By using this definition, the goal of <a href="https://en.wikipedia.org/wiki/Vision_Zero_(New_York_City)" target="_blank">Vision Zero</a> is to eliminate all serious traffic collisions in New York City by 2024, and the analysis presented here could therefore help achieve this goal. For a future project, it would be interesting to look more into lethal car collisions, but there is *luckily* not a lot of data on that.  
+                For this project, we defined serious collisions as collisions where people are either injured or killed. By using this definition, the goal of [Vision Zero](https://en.wikipedia.org/wiki/Vision_Zero_(New_York_City)) is to eliminate all serious traffic collisions in New York City by 2024, and the analysis presented here could therefore help achieve this goal. For a future project, it would be interesting to look more into lethal car collisions, but there is *luckily* not a lot of data on that.  
                 
                 
                 **Percentages:**
@@ -110,7 +110,7 @@ app.layout = html.Div(
         dcc.Graph(className="wrapper", figure=figs.alcohol),
         dcc.Markdown("""
         
-        Diving into the contributing factor 'alcohol involment'. In the last plot we see a similar distribution as the one in the explainer notebook, which looked into the total number of collisions. However in the this plot we have restricted it to only count the ones, where alcohol involment has been a contributing factor of the collision. From that plot, we sort of concluded that we could not see any effect over the years of Vision Zero's campaign 'Choices', which taggets driving while influenced by alcohol directly by showing clear images of how wrong it can go. The first plot above shows on the other hand the seriousness of alcohol involment, i.e. given you are in a moter vehical collision, what is the probability of that collision being serious? Looking specifically at collisions that resultet in being serious, we do on the other hans see an effect. The campaign was launched in March 2016 and already in 2017 the change from the previous year was decreased with 2.23 %. The succes of the campaign was on the other hand short, and already in 2018 the probability increased with 1.24 %.
+        Diving into the contributing factor 'Alcohol Involment'. In the last plot we see a similar distribution as the one in the explainer notebook, which looked into the total number of collisions. However in the this plot we have restricted it to only count the ones, where alcohol involment has been a contributing factor of the collision. From that plot, we sort of concluded that we could not see any effect over the years of Vision Zero's campaign 'Choices', which taggets driving while influenced by alcohol directly by showing clear images of how wrong it can go. The first plot above shows on the other hand the seriousness of alcohol involment, i.e. given you are in a moter vehical collision, what is the probability of that collision being serious? Looking specifically at collisions that resultet in being serious, we do on the other hans see an effect. The campaign was launched in March 2016 and already in 2017 the change from the previous year was decreased with 2.23 %. The succes of the campaign was on the other hand short, and already in 2018 the probability increased with 1.24 %.
         
         [Link to Visions Zero campaign 'Choices'](http://www.nyc.gov/html/visionzero/pages/initiatives/choices.shtml)
         """, className='wrapper')
@@ -154,7 +154,28 @@ app.layout = html.Div(
             dcc.Markdown('''
             # Summary 
             
-            Here is where we write the conclusions about the data.
+            To conclude we have analyzed the data as to where and when serious collisions happen and brought it to life using interactive visualizations. 
+
+            In addition, we have also looked at a few specific cases of contributing factors or times of interest (Alcohol Involvement, Driver Inattention/Distraction and COVID-19) to confirm or deny hypotheses we had about the data.
+
+            We will now highlight a few key points: 
+            * Very few collisions are deadly, which is why we can define ‘serious’ as a collision being either injuring or deadly.
+
+            * The highest risk of a collision being serious is found in Brooklyn, and the lowest was found to be in Staten Island.
+
+            * Within the different boroughs we found local pattern in the different zip codes. 
+
+            * We found that the probability of serious crashes is very time dependent, where collisions happening at night are generally more at risk of being serious than crashes around noon. 
+
+            * We saw the contributing factors each had a different effect on the risk of a collision being lethal or serious. Factors that involve pedestrians/cyclists were very dangerous, whereas factor such like ‘Unsafe Lane Changing’ were generally low risk and being serious.
+            
+            * Even though the risk of a collision being serious increased in the beginning of the COVID-19’s surge in New York City, it was shown that this increase was due to the fact that the number of non-serious collisions decreased dramatically.
+            
+            * The effect of Vision Zero campaign ‘Choices’ targeting driving while influenced by alcohol only seemed to have a short term fix in the probability of a collision being serious. 
+            
+            * For the contributing factor ‘distraction’ we saw an increase in 2016. One explanation could be that the National Highway Traffic Safety Administration ended after a year long campaign.
+
+            If you're thinking to yourself "Wow, that was really interesting. Where can I get more?" Then uou should definitely check out our **[Explainer Notebook](http://www.student.dtu.dk/~s164419/explainer.html)** where we talk a bit more about the nitty gritty details of the content of this website.
             '''),
             ],
             className="wrapper",
